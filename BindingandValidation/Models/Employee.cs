f3024233d958af717eca11a3454c3369759e1d76
sakshamkadayat <sakshamkadayat@gmail.com>
2023-07-30 07:06:10 -0700

    Model Binding and Validation in asp.net core

---
+++ BindingandValidation/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BindingandValidation.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Your FullName is Required")]
+        public string FullName { get; set; }
+
+
+        [Required(ErrorMessage = "Your FullName is Required")]
+        public string Address { get; set; }
+
+       
+        [StringLength(10)]
+        public int PhoneNumber { get; set; }
+
+        [StringLength(12)]
+        public int CitizenshipNo { get; set; }
+
+    }
+}
